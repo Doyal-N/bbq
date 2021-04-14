d36e918b732d5150b06d50f02c1e31e9c8b60930
Doyal-N <coin20@protonmail.com>
2021-04-14 16:59:53 +0500

    base tuning devise

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :title, :address, :datetime, presence: true
+  validates :title, length: { maximum: 30 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'events#index'
+  root "welcome#index"
+  devise_for :users
 
   resources :users, only: [:show, :edit, :update], shallow: true do
     resources :events

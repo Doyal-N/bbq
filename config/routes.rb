1f91d72318f9e39d073f4f345f922871736fe717
Doyal-N <coin20@protonmail.com>
2021-04-15 00:18:33 +0500

    refact

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root "welcome#index"
   devise_for :users
+  root "events#index"
 
-  resources :users, only: [:show, :edit, :update], shallow: true do
-    resources :events
-  end
+  resources :events
+  resources :users, only: [:show, :edit, :update]
 end

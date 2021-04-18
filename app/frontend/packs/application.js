e17beafe3e1dd467f0f91bfddb786dfdb63dae70
Doyal-N <coin20@protonmail.com>
2021-04-18 10:35:48 +0500

    add flatpickr

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,13 +2,20 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
-
 Rails.start()
-
-import 'packs/notification'
 
 import "stylesheets/application"
 import "@fortawesome/fontawesome-free/js/all"
 const images = require.context('../images', true)
+import "flatpickr/dist/flatpickr.min.css"
+
+import 'packs/notification'
+import flatpickr from "flatpickr"
+
+document.addEventListener('DOMContentLoaded', () => {
+  flatpickr(".datepicker", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+  })
+})

753762c159b01ac08c079a410ecdbe559d3c4fc2
Doyal-N <coin20@protonmail.com>
2021-04-19 17:45:43 +0500

    add coment and subscription models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
+  has_many :subscribers, through: :subscriptions, source: :user
 
   validates :title, :address, :datetime, presence: true
   validates :title, length: { maximum: 30 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 
-  validates :name, length: { maximum: 15 }
+  validates :name, presence: true, length: { maximum: 15 }
 end

12db60c1c2ae4d01bb7ae94677b350787dfd7e2e
Doyal-N <coin20@protonmail.com>
2021-04-21 18:47:08 +0500

    add avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up,
-      keys: %i[name email password password_confirmation]
+      :sign_up, keys: %i[name email password password_confirmation]
     )
   end
 

a66333ca0d2256da97594c328086d152ea91bd7d
Doyal-N <61628005+Doyal-N@users.noreply.github.com>
2021-04-21 17:58:51 +0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up,
-      keys: %i[name password password_confirmation current_password]
+      keys: %i[name email password password_confirmation]
     )
   end
 

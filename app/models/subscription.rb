02a0feaa2d1020a9a66bed30074f1f564c0b75ec
Doyal-N <coin20@protonmail.com>
2021-04-22 11:43:37 +0500

    add used_email validation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,11 +2,17 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: { with: /\A\w+@\w+\.[a-z]+\z/ }, unless: -> { user.present? }
-  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
-  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
-  validate :stranger_email, unless: -> { user.present? }
+  with_options if: -> { user.present? } do
+    validates :user, uniqueness: { scope: :event_id }
+    validate :used_email
+  end
+
+  with_options unless: -> { user.present? } do
+    validates :user_name, presence: true
+    validates :user_email, presence: true, format: { with: /\A\w+@\w+\.[a-z]+\z/ }
+    validates :user_email, uniqueness: { scope: :event_id }
+    validate :stranger_email
+  end
 
   def user_name
     if user.present?
@@ -30,5 +36,11 @@ class Subscription < ApplicationRecord
     return unless event.subscribers.find_by(email: user_email)
 
     errors.add(:user_email, :taken_email)
+  end
+
+  def used_email
+    return unless self.class.find_by(user_email: user.email)
+
+    errors.add(:user, :taken_email)
   end
 end

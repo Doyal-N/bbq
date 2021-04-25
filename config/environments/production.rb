e89414873b0cf39806b8aa9936f82501c2750200
Doyal-N <coin20@protonmail.com>
2021-04-25 19:38:58 +0500

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { host: 'mighty-sands-46985.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
 
   config.action_mailer.delivery_method = :smtp
 

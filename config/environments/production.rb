86e73d81297d8bafb7f6ac948cbc0ede13580354
Doyal-N <coin20@protonmail.com>
2021-04-26 17:31:33 +0500

    mailer tunes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,13 +76,10 @@ Rails.application.configure do
 
   config.active_storage.service = :amazon
 
-  config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
-
-  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',

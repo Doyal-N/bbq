84dfebecfa85d797ec92efea58570895d8d30c9e
Doyal-N <coin20@protonmail.com>
2021-04-26 20:35:14 +0500

    fix

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,5 +1,5 @@
 Mailjet.configure do |config|
   config.api_key = ENV['MAILJET_API_KEY']
   config.secret_key = ENV['MAILJET_SECRET_KEY']
-  config.api_version = 'v3.1'
+  config.default_from = ENV['MY_MAIL']
 end

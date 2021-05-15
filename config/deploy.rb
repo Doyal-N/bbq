f958210ce559787d5ddf744d0a2648e23bd72bc4
Doyal-N <coin20@protonmail.com>
2021-05-15 13:25:03 +0500

    tune cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,27 @@ lock '~> 3.16.0'
 
 set :application, 'fin-catalog.ru'
 set :repo_url, 'git@github.com:Doyal-N/bbq.git'
+set :deploy_user, 'deploy'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/home/deploy/www/'
+append :linked_files, 'config/master.key'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'storage'
+
+set :keep_releases, 2
+
+namespace :deploy do
+  desc 'Restart app'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+end
+
+after 'deploy:publishing', :restart
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -19,20 +34,11 @@ set :repo_url, 'git@github.com:Doyal-N/bbq.git'
 # Default value for :pty is false
 # set :pty, true
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure

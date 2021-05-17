65a6a7835925c1653c3a3288d3c17ef08d6e5b12
Doyal-N <coin20@protonmail.com>
2021-05-17 12:57:38 +0500

    event_policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include Pundit
 
   helper_method :current_user_can_edit?
 
@@ -12,5 +13,15 @@ class ApplicationController < ActionController::Base
   def current_user_can_edit?(model)
     user_signed_in? && (model.user == current_user ||
       (model.try(:event).present? && model.event.user == current_user))
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+
+    flash[:alert] = t('pundit.not_authorized')
+    redirect_to(request.referrer || root_path)
   end
 end
--- test/mailers/previews/notify_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-class NotifyMailerPreview < ActionMailer::Preview
-  def added_image
-    NotifyMailer.added_image(Image.first, 'test@mail.ru')
-  end
-
-  def added_comment
-    NotifyMailer.added_comment(Comment.first, 'test@mail.ru')
-  end
-end

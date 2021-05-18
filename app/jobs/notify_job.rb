9be98eaf04bdd496a805c418500a1611a74db1a9
Doyal-N <coin20@protonmail.com>
2021-05-18 19:53:02 +0500

    resque

--- app/jobs/notify_job.rb
+++ app/jobs/notify_job.rb
@@ -2,6 +2,6 @@ class NotifyJob < ApplicationJob
   queue_as :default
 
   def perform(record)
-    NotifyService.send_mail_about_new_record(record.id)
+    NotifyService.send_mail_about_new_record(record)
   end
 end

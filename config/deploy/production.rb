0c9acbbd18cc5053d0685cebc5551b08f162c24c
Doyal-N <coin20@protonmail.com>
2021-05-19 00:13:20 +0500

    resque

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'fin-catalog.ru', user: 'deploy', roles: %w[app db web resque_worker]
+server 'fin-catalog.ru', user: 'deploy', roles: %w[app db web]
 
 set :resque_environment_task, true
 set :workers, { "#{fetch(:application)}*" => 1 }

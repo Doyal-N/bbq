ab82d188240365998eb0a159b910f5b7f06d364e
Doyal-N <coin20@protonmail.com>
2021-05-23 13:48:48 +0500

    Change devise.rb :construction:

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,4 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token, only: :facebook
-
   def facebook
     @user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
 
